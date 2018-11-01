531b926781bbf7572cbd1ebfbb1fbc9aac2a07ec
Alexandr Morozov <mopoz07@gmail.com>
2018-11-01 18:08:19 +0200

    dynamic components

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,20 @@
     <div class="container">
       <div class="row">
         <div class="col">
-          <app-quote>
+          <button @click="selectedComponent='appQuote'">Quote</button>
+          <button @click="selectedComponent='appAuthor'">Author</button>
+          <button @click="selectedComponent='appNew'">New</button>
+          <hr>
+          <p>{{ selectedComponent }}</p>
+          <keep-alive>
+            <component :is="selectedComponent">
+              <p>Default content</p>
+            </component>
+          </keep-alive>
+          <!-- <app-quote>
             <h2 slot="title">{{ quoteTitle }}</h2>
             <p>A wonderfull quote!</p>
-          </app-quote>          
+          </app-quote> -->
         </div>
       </div>
     </div>
@@ -13,15 +23,20 @@
 
 <script>
 import Quote from './components/Quote.vue';
+import Author from './components/Author.vue';
+import New from './components/New.vue';
 
 export default {
   data() {
     return {
-      quoteTitle: 'The Quote'
+      quoteTitle: 'The Quote',
+      selectedComponent: 'appQuote'
     };
   },
   components: {
-    appQuote: Quote
+    appQuote: Quote,
+    appAuthor: Author,
+    appNew: New
   }
 
 }
