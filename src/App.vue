4bcf8abc9d5fb21837d58ea60c42c34bd287135c
Alexandr Morozov <mopoz07@gmail.com>
2018-11-02 19:32:31 +0200

    animations#1

--- src/App.vue
+++ src/App.vue
@@ -2,43 +2,144 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Filters & Mixins</h1>
-        <p>{{ text | toUppercase | to-lowercase }}</p>
+        <h1>Animations</h1>
         <hr>
-        <button @click="fruits.push('Berries')">Add New Item</button>
-        <input v-model="filterText">
-        <ul>
-          <li v-for="fruit in filteredFruits">{{ fruit }}</li>
-        </ul>
+        <select v-model="alertAnimation" class="form-control">
+          <option value="fade">Fade</option>
+          <option value="slide">Slide</option>
+        </select>
+        <br>
+        <button class="btn btn-primary" @click="show = !show">Show Allert</button>
+        <br><br>
+        <transition :name="alertAnimation">
+          <div class="alert alert-info" v-show="show">This is some info</div>
+        </transition>  
+        <transition name="slide" type="animation">
+          <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition> 
+        <transition 
+                appear
+                enter-active-class="animated bounce"
+                leave-active-class="animated shake"
+        >
+          <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>  
+
+        <transition :name="alertAnimation" mode="out-in">
+          <div class="alert alert-info" v-if="show" key="info">This is some Info</div>
+          <div class="alert alert-warning" v-else key="warning">This is some Warning</div>
+        </transition>  
         <hr>
-        <app-list></app-list>
+
+        <button class="btn btn-primary" @click="load = !load">Load / Remove Element</button>
+        <br><br>
+        <transition
+          @before-enter="beforeEnter"
+          @enter="enter"
+          @after-enter="afterEnter"
+          @enter-cancelled="enterCancelled"
+
+          @before-leave="beforeLeave"
+          @leave="leave"
+          @after-leave="afterLeave"
+          @leave-cancelled="leaveCancelled"
+          >
+          <div style="width: 100px; height: 100px; background-color: lightgreen" v-if="load"></div>
+        </transition>  
+
       </div>
     </div>      
   </div>
 </template>
 
 <script>
-import List from './List.vue';
-import { fruitMixin } from './fruitMixin.js';
 
 export default {
-  mixins: [fruitMixin],
   data() {
     return {
-      text: 'Hello there!',
+      show: false,
+      load: true,
+      alertAnimation: 'fade'
     };
   },
-  filters: {
-    toUppercase(value) {
-      return value.toUpperCase();
+  methods: {
+    beforeEnter(el) {
+      console.log('beforeEnter');
+    },
+    enter(el, done) {
+      console.log('enter');
+      done();
+    },
+    afterEnter(el) {
+      console.log('afterEnter');
+    },
+    enterCancelled(el) {
+      console.log('enterCancelled');
+    },
+    beforeLeave(el) {
+      console.log('beforeLeave');
+    },
+    leave(el, done) {
+      console.log('leave');
+      done();
+    },
+    afterLeave(el) {
+      console.log('afterLeave');
+    },
+    leaveCancelled(el) {
+      consle.log('leaveCancelled');
     }
-  },
-  components: {
-    appList: List
   }
 }
 </script>
 
 <style>
+  .fade-enter {
+    opacity: 0;
+  }
+  .fade-enter-active {
+    transition: opacity 1s;
+  }
+  .fade-leave {
+    /* opacity: 1; */
+  }
+  .fade-leave-active {
+    opacity: 0;
+    transition: opacity 1s;
+  }
+
+  .slide-enter {
+    opacity: 0;
+  }
+  .slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+    transition: opacity .5s;
+  }
+  .slide-leave {
+    
+  }
+  .slide-leave-active {
+    animation: slide-out 1s ease-out forwards;
+    opacity: 0;
+    transition: opacity 1s;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateY(20px);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(20px);
+    }
+  }
 
 </style>
